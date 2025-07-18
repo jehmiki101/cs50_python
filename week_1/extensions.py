625294f6ef11d17ce8fe902c1e073b72c4c668a6
jehmiki101 <jmikihara@gmail.com>
2025-07-18 13:31:20 -0300

    exercicio extensoes de arquivos com if e elses com substring em texto

--- week_1/extensions.py
+++ week_1/extensions.py
@@ -1,18 +1,18 @@
 def main():
-    text = input("File name: ")
+    text = input("File name: ").lower()
     if ".gif" in text:
         print("image/gif")
     elif ".jpg" in text:
-        print("image/jpg")
-    elif ".jpeg":
         print("image/jpeg")
-    elif "png":
+    elif ".jpeg" in text:
+        print("image/jpeg")
+    elif ".png" in text:
         print("image/png")
-    elif "pdf":
+    elif ".pdf" in text:
         print("application/pdf")
-    elif "txt":
+    elif ".txt" in text:
         print("text/plain")
-    elif "zip":
+    elif ".zip" in text:
         print("application/zip")
     else:
         print("application/octet-stream")

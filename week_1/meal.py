82583a75622f92d7c9510d4bc8684a3867982804
femow101 <felipemu92@gmail.com>
2025-07-31 18:13:08 -0300

    funcao converter para float com metodo split, if elif

--- week_1/meal.py
+++ week_1/meal.py
@@ -10,21 +10,22 @@ calculo de hora: 1:50
 '''
 
 def main():
-    time_input = input("What time is it? ").split(":")
-    if 7 < convert(time_input) < 8:
+    time_input = input("What time is it? ")
+    hour = convert(time_input)
+    if 7 <= hour <= 8:
         print("breakfast time")
-    elif 12 < convert(time_input) < 13:
+    elif 12 <= hour <= 13:
         print("lunch time")
-    if 18 < convert(time_input) < 19:
+    elif 18 <= hour <= 19:
         print("dinner time")
 
 
 def convert(time):
-    time_float_hour = float(time[0])
-    time_float_min = float(time[1])
-    hour_to_number = (time_float_min / 60) + time_float_hour
-    return hour_to_number
+    hours, minutes = time.split(":")
+    time_float_hour = float(hours)
+    time_float_min = float(minutes) / 60
+    return time_float_hour + time_float_min
 
-    
+
 if __name__ == "__main__":
     main()
